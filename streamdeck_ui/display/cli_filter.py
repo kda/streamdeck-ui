f147a1dd3c35ff95632d74aeb659a0c62fd868cd
Kevin Athey <kda@pobox.com>
2023-06-15 13:46:32 -0700

    improved by separating state from image generation

--- streamdeck_ui/display/cli_filter.py
+++ streamdeck_ui/display/cli_filter.py
@@ -19,14 +19,13 @@ class CliFilter(filter.Filter):
     def __init__(self, args: list[str]):
         super(CliFilter, self).__init__()
         self.args_ = args
-        print(self.args_)
+        #print(self.args_)
 
     def initialize(self, size: Tuple[int, int]):
         self.image = Image.new("RGB", size)
         spec = importlib.util.spec_from_file_location("junk", self.args_[0])
         self.mod_ = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(self.mod_)
-
 
     def transform(self, get_input: Callable[[], Image.Image], get_output: Callable[[int], Image.Image], input_changed: bool, time: Fraction) -> Tuple[Image.Image, int]:
         """
@@ -38,10 +37,14 @@ class CliFilter(filter.Filter):
         #inputfp = tempfile.TemporaryFile()
         #print('inputfp.name: %s' % (inputfp.name))
 
+        state = self.mod_.detectState(self.args_[1:])
+        hashcode = hash((self.__class__,) + tuple(self.args_) + (state,))
+        existing_image = get_output(hashcode)
+        if existing_image:
+            return (existing_image, hashcode)
 
+        print('preparing image: ', self.args_, state)
         input_image = get_input()
-        (self.image, hashcode) = self.mod_.prepareImage(input_image, self.args_[1:])
+        input_image = self.mod_.prepareImage(input_image, state)
 
-        #if not input_changed:
-        #    return (None, hashcode)
-        return ((self.image), hashcode)
+        return (self.image, hashcode)
